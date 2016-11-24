3dee66b58c349d2dd13f77d8188bb819be0eccee
panlinlin <panlinlinpeter@gmail.com>
2016-11-24 22:37:46 +0800

    init

---
+++ CodingNight/app/src/main/java/cn/thundersoft/codingnight/fragment/BaseFragment.java
@@ -0,0 +1,30 @@
+package cn.thundersoft.codingnight.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by pandroid on 11/24/16.
+ */
+
+public class BaseFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        TextView mainTV = new TextView(getContext());
+        mainTV.setText(getArguments().getString("demo"));
+        return mainTV;
+    }
+}
