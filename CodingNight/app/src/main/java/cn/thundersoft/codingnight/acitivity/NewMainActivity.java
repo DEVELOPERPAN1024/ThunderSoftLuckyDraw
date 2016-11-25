509364cb7ba2c02dd2f5e4566b4f5895468a2d5e
panlinlin <panlinlinpeter@gmail.com>
2016-11-25 19:08:14 +0800

    mod main

--- CodingNight/app/src/main/java/cn/thundersoft/codingnight/acitivity/NewMainActivity.java
+++ CodingNight/app/src/main/java/cn/thundersoft/codingnight/acitivity/NewMainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
 import android.widget.LinearLayout;
 
 import butterknife.Bind;
@@ -14,7 +18,7 @@ import cn.thundersoft.codingnight.R;
  * Created by pandroid on 11/25/16.
  */
 
-public class NewMainActivity extends AppCompatActivity {
+public class NewMainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Bind(R.id.main_data_cv)
     CardView mMainDataCV;
@@ -30,9 +34,39 @@ public class NewMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_new);
         ButterKnife.bind(this);
+        initAnimation();
+        initView();
     }
 
-    
+    private void initView() {
+        mMainDataCV.setOnClickListener(this);
+        mMainAwardCV.setOnClickListener(this);
+        mMainLuckyDrawCV.setOnClickListener(this);
+    }
+
+    private void initAnimation() {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.enter_from_right);
+        LayoutAnimationController controller = new LayoutAnimationController(animation);
+        controller.setOrder(LayoutAnimationController.ORDER_NORMAL);
+        mMainItemLL.setLayoutAnimation(controller);
+    }
 
 
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.main_data_cv: {
+
+            }
+            case R.id.main_award_cv: {
+
+            }
+            case R.id.main_luckydraw_cv: {
+
+            }
+            default:
+                break;
+        }
+    }
 }
