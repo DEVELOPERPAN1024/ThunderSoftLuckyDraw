6d5ea192ac9ceea41e59d6767557a5bdac327552
GreenShadow <569027851@qq.com>
2017-01-19 20:10:37 +0800

    revert SpecialAwardActionProvider

--- CodingNight/app/src/main/java/cn/thundersoft/codingnight/ui/SpecialAwardActionProvider.java
+++ CodingNight/app/src/main/java/cn/thundersoft/codingnight/ui/SpecialAwardActionProvider.java
@@ -1,4 +1,4 @@
-package cn.thundersoft.codingnight.db;
+package cn.thundersoft.codingnight.ui;
 
 import android.content.Context;
 import android.content.Intent;
@@ -27,10 +27,6 @@ public class SpecialAwardActionProvider extends ActionProvider {
 
     @Override
     public View onCreateActionView() {
-//        View actionView = LayoutInflater.from(context)
-//                .inflate(R.layout.popupwindow_for_add_btn, null);
-//        initPopupView(actionView);
-//        return actionView;
         return null;
     }
 
@@ -47,25 +43,5 @@ public class SpecialAwardActionProvider extends ActionProvider {
                 .setIcon(R.drawable.ic_launcher);
         subMenu.addSubMenu("现金红包")
                 .setIcon(R.drawable.envelop_icon);
-    }
-
-    private void initPopupView(View popView) {
-        TextView mNormalAwardTV = (TextView) popView.findViewById(R.id.add_normal_award_tv);
-        final TextView mSpecialAwardTV = (TextView) popView.findViewById(R.id.add_special_award);
-        mNormalAwardTV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(context, AddAwardActivity.class);
-                context.startActivity(intent);
-
-            }
-        });
-        mSpecialAwardTV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(context, AddSpecialActivity.class);
-                context.startActivity(intent);
-            }
-        });
     }
 }
